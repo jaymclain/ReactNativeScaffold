38b297379135e67a9d5deb1299f24ee91331ea46
Jay McLain <jay_mclain@roundrockisd.org>
2024-11-08 12:21:52 -0600

    Add code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   preset: 'react-native',
+  collectCoverage: true,
+  collectCoverageFrom: [
+      "**/*.{ts,tsx,js,jsx}",
+      "!index.js",
+      "!**/coverage/**",
+      "!**/node_modules/**",
+      "!.eslintrc.js",
+      "!.prettierrc.js",
+      "!**/*.config.js"
+    ],
+    coverageReporters: [ "json-summary", "text", "cobertura" ]
 };
