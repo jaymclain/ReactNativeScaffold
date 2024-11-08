d426960750918e38415f63e1e70ea45b5bffd35d
Jay McLain <jay_mclain@roundrockisd.org>
2024-11-08 11:51:52 -0600

    Update testing library

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -2,16 +2,11 @@
  * @format
  */
 
-import 'react-native';
-import React from 'react';
+import { render, screen } from '@testing-library/react-native';
 import App from '../App';
 
-// Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
+test('renders App component', () => {
+  render(<App />);
+  const textElement = screen.getByText('Stay in touch with the community, join in on Q&As and more by following React Native on X.');
+  expect(textElement).toBeTruthy();
 });
